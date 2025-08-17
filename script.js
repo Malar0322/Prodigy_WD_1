a7b87f9e069203cdf7739c09fa5e880d803c7d1a
Malar0322 <prajaktalaxmi11@gmail.com>
2025-08-17 15:56:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get navbar
+const navbar = document.getElementById('navbar');
+
+// Add scroll event
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
